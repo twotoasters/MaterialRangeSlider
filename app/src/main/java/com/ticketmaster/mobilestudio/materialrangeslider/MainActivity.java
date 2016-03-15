4c12b0903255743c0ca563d053f2146d771c31f9
Patrick Jackson <patjackson52@gmail.com>
2016-03-15 14:09:32 -0400

    updated android gradle plugin, butterknife, target sdk, and build tools.

--- app/src/main/java/com/ticketmaster/mobilestudio/materialrangeslider/MainActivity.java
+++ app/src/main/java/com/ticketmaster/mobilestudio/materialrangeslider/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
+        ButterKnife.bind(this);
     }
 
 }
